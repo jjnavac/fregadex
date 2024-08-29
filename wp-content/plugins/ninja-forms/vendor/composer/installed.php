7cd7b317040911f3eb819df474df0476d5451742
Junior De Jesus Nava Contreras <citven@gmail.com>
2024-08-29 09:55:41 -0400

    actualizacion 29/08

---
+++ wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-e0c3c8f40dc654036adc4f73f1ab9edd71c513d0',
+        'version' => 'dev-e0c3c8f40dc654036adc4f73f1ab9edd71c513d0',
+        'reference' => 'e0c3c8f40dc654036adc4f73f1ab9edd71c513d0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-e0c3c8f40dc654036adc4f73f1ab9edd71c513d0',
+            'version' => 'dev-e0c3c8f40dc654036adc4f73f1ab9edd71c513d0',
+            'reference' => 'e0c3c8f40dc654036adc4f73f1ab9edd71c513d0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
